ba351418d657f5d93fb7e4fc9825bfa6030e35f2
artem-p <artempugachev1@gmail.com>
2017-10-19 17:56:40 +0300

    add blank android library

---
+++ jokesactivity/src/main/java/ru/artempugachev/jokesactivity/JokesActivity.java
@@ -0,0 +1,13 @@
+package ru.artempugachev.jokesactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JokesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
+    }
+}
